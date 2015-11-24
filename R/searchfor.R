45149f1255faceca11cd36e2a093ced36a3f28c1
Michael Sumner <mdsumner@gmail.com>
2015-11-25 08:43:36 +1100

    first commit

---
+++ R/searchfor.R
@@ -0,0 +1,39 @@
+## search for
+
+
+#' Search for pattern 
+#'
+#' @param text corpus
+#' @param pattern  search for
+#'
+#' @return string
+#' @export
+#' @importFrom utils person
+searchfor <- function(text, pattern = NULL) {
+  if (is.null(pattern)) pattern <- .searchPatterns()
+  finds <- setNames(vector("list", length(pattern)), pattern)
+  for (i in seq_along(pattern)) {
+    finds[[i]] <-   sort(unique(grep(pattern[i], unlist(strsplit(text, "\\s+")), value = TRUE)))
+  }
+  finds
+}
+
+
+.searchPatterns <- function() {
+  unlist(strsplit(
+  "ise
+  ize
+  ising
+  izing
+  lyz
+  lys
+  vour
+  vor
+  elle
+  ele
+  eli
+  elli
+  vior
+  viour
+  ", "\\s+"))
+}
